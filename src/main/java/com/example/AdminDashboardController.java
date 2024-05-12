f81c2da1d332a2e076fbf61db384fa0ad5bc1006
narful <ayat-nour.hachmi@uir.ac.ma>
2024-05-12 17:24:01 +0100

    fonttttt for logo

--- src/main/java/com/example/AdminDashboardController.java
+++ src/main/java/com/example/AdminDashboardController.java
@@ -1,21 +1,40 @@
 package com.example;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class AdminDashboardController {
 
     @FXML
     private AnchorPane menuitems;
 
+    void initialize() {
+
+        menuitems.setVisible(false);
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), menuitems);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), menuitems);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+    }
+
     @FXML
     void hidemenu(MouseEvent event) {
-
+        menuitems.setVisible(false);
     }
 
     @FXML
     void showmenu(MouseEvent event) {
+
+        menuitems.setVisible(true);
 
     }
 
