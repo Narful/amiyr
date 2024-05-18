383a8987b12007a512cc734b64838ccd114df159
narful <ayat-nour.hachmi@uir.ac.ma>
2024-05-18 16:10:25 +0100

    add menu.java

---
+++ src/main/java/com/example/Menu.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Menu {
+
+   public int idMenu;
+
+   public Plat[] plats;
+
+   public Boolean ajouter(int idPlat) {
+      // TODO: implement
+      return null;
+   }
+
+   public Boolean retirer(int idPlat) {
+      // TODO: implement
+      return null;
+   }
+
+   public Boolean genererPdf() {
+      // TODO: implement
+      return null;
+   }
+
+   public Boolean consulter() {
+      // TODO: implement
+      return null;
+   }
+
+}
