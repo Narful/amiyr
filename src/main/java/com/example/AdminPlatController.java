df9ec8ba851893572fa46b2502acc3d49f0db0c2
narful <ayat-nour.hachmi@uir.ac.ma>
2024-06-02 14:07:33 +0100

    ok

---
+++ src/main/java/com/example/AdminPlatController.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminPlatController {
+
+    @FXML
+    private ImageView imgp;
+
+    @FXML
+    private AnchorPane menuitems;
+
+    @FXML
+    void hidemenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showmenu(MouseEvent event) {
+
+    }
+
+}
