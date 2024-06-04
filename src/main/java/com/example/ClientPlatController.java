ac58ac5f5cb4b6832123d2145efabbb139d26e2f
XenonMido <axi.elasri@gmail.com>
2024-06-04 11:08:11 +0100

    added minor updates

--- src/main/java/com/example/ClientPlatController.java
+++ src/main/java/com/example/ClientPlatController.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -9,8 +10,20 @@ import java.io.ByteArrayInputStream;
 import java.sql.*;
 
 public class ClientPlatController {
-    @FXML
-    private ImageView logoAmiyr;
+
+
+    for(int i=0;i<5;i++){
+        try {
+            FXMLLoader load = new FXMLLoader();
+            load.getClass().getResource("cartePlatClient.fxml");
+            AnchorPane pane = load.load();
+            CartePlatClientController cardC = load.getController();
+            cardC.setCardData("name","categorie", 15.2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     
 
