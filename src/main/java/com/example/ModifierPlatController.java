d89e86fc1b9db5b81ee001e7f7c661f35dfee406
narful <ayat-nour.hachmi@uir.ac.ma>
2024-06-05 02:13:26 +0100

    ModifierPlat added

---
+++ src/main/java/com/example/ModifierPlatController.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ModifierPlatController {
+
+    @FXML
+    private ComboBox<?> categorie;
+
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private ImageView imgModif;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField prix;
+
+    @FXML
+    private Spinner<?> qte;
+
+    @FXML
+    void ModifIMG(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveModifPlat(ActionEvent event) {
+
+    }
+
+}
