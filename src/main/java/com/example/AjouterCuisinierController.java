3a43dda5fa4db76212d1038f601f56ca356d6f95
narful <ayat-nour.hachmi@uir.ac.ma>
2024-06-05 03:54:35 +0100

    AjouterCuisinier Added

---
+++ src/main/java/com/example/AjouterCuisinierController.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AjouterCuisinierController {
+
+    @FXML
+    private TextField confEmailCuisi;
+
+    @FXML
+    private TextField emailCuisi;
+
+    @FXML
+    private TextField nomCuisi;
+
+    @FXML
+    private TextField prenomCuisi;
+
+}
